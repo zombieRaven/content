f3229af36c05f24960120675b54fad226a85ea60
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-10-29 13:38:38 +0200

    Added split in time stamp

--- Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
+++ Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
@@ -7,15 +7,16 @@ USER = demisto.args().get('user')
 REPO = demisto.args().get('repo')
 LABEL = demisto.args().get('label')
 time_range_start, _ = parse_date_range(FETCH_WINDOW)
-reg = re.compile("\.\d{6}$")
-timestamp, _ = reg.subn('', time_range_start.isoformat())
-query = f'repo:{USER}/{REPO} is:pr is:open label:{LABEL} updated:>{timestamp}'
+timestamp = time_range_start.isoformat().split('T')[0]
+query = f'repo:{USER}/{REPO} is:issue is:open label:{LABEL} updated:>{timestamp}'
+demisto.results(query)
 
 try:
     # Calling a command - returns a list of one or more entries
     cmd_res = demisto.executeCommand('GitHub-search-issues', {'query': query})
+    demisto.results(cmd_res)
     issues = demisto.get(cmd_res[0], 'Contents.items')
     demisto.results(issues)
 
 except Exception as e:
-    return_error(e)
+    demisto.results(e)
