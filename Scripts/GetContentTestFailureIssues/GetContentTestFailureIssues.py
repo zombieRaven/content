5b829acf142582387d2df7b60adf06861f4c53e5
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-10-29 16:20:15 +0200

    Added limit

--- Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
+++ Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
@@ -6,6 +6,7 @@ FETCH_WINDOW = demisto.args().get('fetch_window', '2 months')
 USER = demisto.args().get('user')
 REPO = demisto.args().get('repo')
 LABELS = argToList(demisto.args().get('label'))
+LIMIT = demisto.args().get('limit')
 time_range_start, _ = parse_date_range(FETCH_WINDOW)
 timestamp = time_range_start.isoformat().split('T')[0]
 labels = str([f'label:\"{label}\"' for label in LABELS])[1:-1].replace(',', '').replace('\'', '')
@@ -15,7 +16,7 @@ demisto.results(query)
 
 try:
     # Calling a command - returns a list of one or more entries
-    cmd_res = demisto.executeCommand('GitHub-search-issues', {'query': query})
+    cmd_res = demisto.executeCommand('GitHub-search-issues', {'query': query, 'limit': {LIMIT}})
     demisto.results(cmd_res)
     issues = demisto.get(cmd_res[0], 'Contents.items')
     demisto.results(issues)
