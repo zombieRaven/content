d039695e44b5daf3b9ea9717493d27e24c02ba50
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-10-29 16:18:32 +0200

    Added label interpolation

--- Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
+++ Scripts/GetContentTestFailureIssues/GetContentTestFailureIssues.py
@@ -5,10 +5,12 @@ from CommonServerUserPython import *
 FETCH_WINDOW = demisto.args().get('fetch_window', '2 months')
 USER = demisto.args().get('user')
 REPO = demisto.args().get('repo')
-LABEL = demisto.args().get('label')
+LABELS = argToList(demisto.args().get('label'))
 time_range_start, _ = parse_date_range(FETCH_WINDOW)
 timestamp = time_range_start.isoformat().split('T')[0]
-query = f'repo:{USER}/{REPO} is:issue is:open label:{LABEL} updated:>{timestamp}'
+labels = str([f'label:\"{label}\"' for label in LABELS])[1:-1].replace(',', '').replace('\'', '')
+
+query = f'repo:{USER}/{REPO} is:issue is:open {labels} updated:>{timestamp}'
 demisto.results(query)
 
 try:
