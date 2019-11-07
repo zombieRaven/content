813b7c0dd5665d05240ff004fc2bba1e371406f5
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-11-07 17:33:51 +0200

    s

--- Tests/GetTestsStatistics/GetTestsStatistics.py
+++ Tests/GetTestsStatistics/GetTestsStatistics.py
@@ -9,17 +9,48 @@ urllib3.disable_warnings()
 
 CONF_JSON_PATH = 'https://raw.githubusercontent.com/demisto/content/master/Tests/conf.json'
 
+"""
+LOGIC
+
+"""
+# Data from conf.json
 conf_json = requests.request('GET', CONF_JSON_PATH, verify=False).json()
-
 skipped_test = conf_json.get('skipped_tests')
-total_test_count = conf_json.get('tests')
-demisto.results(skipped_test.len())
-demisto.results(total_test_count.len())
+tests_in_conf = conf_json.get('tests')
+skipped_integrations = conf_json.get('skipped_integrations')
 
-# demisto.results(conf_json)
-# skipped_test = conf_json.get('skipped_tests')
-# total_test_count = conf_json.get('tests')
-#
+# Getting all integrations from latest id_set.json
+artifacts = demisto.executeCommand('circleci-get-latest-artifacts', {'branch': 'master', 'project': 'content'})
+for entry in artifacts:
+    # demisto.results(json.dumps())
+    artifact = entry.get('Contents')
+    if 'id_set.json' in artifact.get('pretty_path'):
+        id_set_url = artifact.get('url')
+id_set = requests.request('GET', id_set_url, verify=False).json()
+id_set_integrations = id_set.get('integrations')
+integrations_names = [list(integration_obj.keys())[0] for integration_obj in id_set_integrations]
+integrations_names_set = (set(integrations_names))
+
+
+demisto.results(f'All integrations count: {len(integrations_names_set)}')
+
+# Removing integrations that are skipped in conf.json
+for integration in skipped_integrations:
+    if integration != '_comment':
+        integrations_names_set.remove(integration)
+
+# Removing integrations that have a skipped test conf.json
+for entry in tests_in_conf:
+    test_playbook = entry.get('playbookID')
+    integrations = entry.get('integrations')
+    if integrations:
+        integrations_list = integrations if isinstance(integrations, list) else [integrations]
+        if test_playbook in skipped_test:
+            for integration in integrations_list:
+                integrations_names_set.remove(integration)
+
+demisto.results(f'All integrations count: {len(integrations_names_set)}')
+
 # data = [
 #     {"name": "Skipped Tests", "data": [skipped_test]},
 #     {"name": "Skipped Tests", "data": [total_test_count]}
