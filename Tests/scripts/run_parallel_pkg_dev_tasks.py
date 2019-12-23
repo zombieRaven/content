d8dda265cd1af6580c7fdc50ec723984671a8512
rsagi <rsagi@paloaltonetworks.com>
2019-12-23 16:33:40 +0200

    testing changes

--- Tests/scripts/run_parallel_pkg_dev_tasks.py
+++ Tests/scripts/run_parallel_pkg_dev_tasks.py
@@ -3,6 +3,8 @@ import os
 import sys
 import subprocess
 import concurrent.futures
+import yaml
+import glob
 from typing import List, Optional, Tuple
 from pkg_dev_test_tasks import get_dev_requirements
 
@@ -12,8 +14,13 @@ sys.path.append(CONTENT_DIR)
 from Tests.test_utils import print_color, LOG_COLORS  # noqa: E402
 
 
-def run_dev_task(pkg_dir: str, params: Optional[List[str]]) -> Tuple[subprocess.CompletedProcess, str]:
-    args = ['demisto-sdk lint', '-d', pkg_dir, '-v']
+def run_dev_task(pkg_dir: str, params: Optional[List[str]], req3, req2) -> Tuple[subprocess.CompletedProcess, str]:
+    if get_image_num(pkg_dir) == 3.7:
+        req = req3
+    else:
+        req = req2
+
+    args = ['demisto-sdk lint', '-d', pkg_dir, '-v', '--requirements', req]
     if params:
         args.extend(params)
     cmd_line = " ".join(args)
@@ -21,6 +28,71 @@ def run_dev_task(pkg_dir: str, params: Optional[List[str]]) -> Tuple[subprocess.
     cmd_line += r" 2> >(sed '/No config file found, using default configuration/d' | sed $'s,.*,\x1B[31m&\x1B[0m,'>&1)"
     res = subprocess.run(cmd_line, text=True, capture_output=True, shell=True, executable='/bin/bash')
     return res, pkg_dir
+
+
+def get_yml_paths_in_dir(project_dir: str, error_msg: str,) -> Tuple[list, str]:
+    """
+    Gets the project directory and returns the path of the first yml file in that directory
+    :param project_dir: string path to the project_dir
+    :param error_msg: the error msg to show to the user in case not yml files found in the directory
+    :return: first returned argument is the list of all yml files paths in the directory, second returned argument is a
+    string path to the first yml file in project_dir
+    """
+    yml_files = glob.glob(os.path.join(project_dir, '*.yml'))
+    if not yml_files:
+        if error_msg:
+            print(error_msg)
+        return [], ''
+    return yml_files, yml_files[0]
+
+
+def get_docker_images(script_obj):
+    imgs = [script_obj.get('dockerimage') or 'demisto/python:1.3-alpine']
+    alt_imgs = script_obj.get('alt_dockerimages')
+    if alt_imgs:
+        imgs.extend(alt_imgs)
+    return imgs
+
+
+def get_python_version(docker_image):
+    """
+    Get the python version of a docker image
+    Arguments:
+        docker_image {string} -- Docker image being used by the project
+    Return:
+        python version as a float (2.7, 3.7)
+    Raises:
+        ValueError -- if version is not supported
+    """
+    stderr_out = -3
+    py_ver = subprocess.check_output(["docker", "run", "--rm", docker_image, "python", "-c",
+                                      "import sys;print('{}.{}'.format(sys.version_info[0], sys.version_info[1]))"],
+                                     universal_newlines=True, stderr=stderr_out).strip()
+
+    py_num = float(py_ver)
+    return py_num
+
+
+def get_image_num(pkg_dir):
+    # load yaml
+    _, yml_path = get_yml_paths_in_dir(pkg_dir, "No Package Dir Given")
+    if not yml_path:
+        return 1
+    with open(yml_path, 'r') as yml_file:
+        yml_data = yaml.safe_load(yml_file)
+    script_obj = yml_data
+    if isinstance(script_obj.get('script'), dict):
+        script_obj = script_obj.get('script')
+    script_type = script_obj.get('type')
+    if script_type != 'python':
+        if script_type == 'powershell':
+            # TODO powershell linting
+            return 0
+
+    dockers = get_docker_images(script_obj)
+    py_num = get_python_version(dockers[0])
+
+    return py_num
 
 
 def should_run_pkg(pkg_dir: str) -> bool:
@@ -76,9 +148,8 @@ def main():
     params = sys.argv[1::]
     fail_pkgs = []
     good_pkgs = []
-    if len(pkgs_to_run) > 1:  # setup pipenv before hand to avoid conflics
-        get_dev_requirements(2.7)
-        get_dev_requirements(3.7)
+    req2 = get_dev_requirements(2.7)
+    req3 = get_dev_requirements(3.7)
     # run CommonServer non parallel to avoid conflicts
     # when we modify the file for mypy includes
     if 'Scripts/CommonServerPython' in pkgs_to_run:
@@ -86,7 +157,7 @@ def main():
         res = run_dev_task('Scripts/CommonServerPython', params)
         handle_run_res(res, fail_pkgs, good_pkgs)
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures_submit = [executor.submit(run_dev_task, dir, params) for dir in pkgs_to_run]
+        futures_submit = [executor.submit(run_dev_task, dir, params, req3, req2) for dir in pkgs_to_run]
         for future in concurrent.futures.as_completed(futures_submit):
             res = future.result()
             handle_run_res(res, fail_pkgs, good_pkgs)
