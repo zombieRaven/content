fffb5cc9983686dd81013ec5e4c192f45b012c8c
yaakovi <syaakovi@paloaltonetworks.com>
2019-12-23 09:33:02 +0200

    added callingContext to demistomock.py

--- Tests/scripts/run_all_pkg_dev_tasks.sh
+++ Tests/scripts/run_all_pkg_dev_tasks.sh
@@ -2,7 +2,7 @@
 
 # Find all package directories and run the pkg_dev_test_tasks.py script. Will run only against
 # packages which have changed comparing in git. Unless SKIP_GIT_COMPARE_FILTER evn var is set.
-# Speciy any parameter to this script that will be passed to the pkg_dev_test_tasks.py as is
+# Speciy any parameter to this script that will be passed to the pkg_dev_test_tasks.py as is.
 
 # Env vars:
 # SKIP_GIT_COMPARE_FILTER: if set will not compare the git commit and run against all pkgs (nightly)
