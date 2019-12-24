6e7f1f335dbd9ce2fd1767dc36da58cdbf53f93b
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-12-24 18:59:35 +0200

    bash script to setup packages for running mitmproxy on instance

--- Tests/scripts/ami_mock_dependencies_setup.sh
+++ Tests/scripts/ami_mock_dependencies_setup.sh
@@ -5,7 +5,9 @@ echo "Installing development requirements for installing python3 on Linux Amazon
 sudo yum -y install gcc bzip2-devel ncurses-devel gdbm-devel xz-devel sqlite-devel 
 sudo yum -y install openssl-devel tk-devel uuid-devel readline-devel zlib-devel libffi-devel
 echo "Downloading python3 source binaries."
+# disable-secrets-detection-start
 wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+# disable-secrets-detection-end
 tar xzf Python-3.8.0.tgz
 cd Python-3.8.0
 ./configure ––enable–optimizations
