e31dd089aef78b2580572c65dec3f52df69f88e6
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-12-30 09:43:52 +0200

    stash and pop

--- Tests/scripts/upload_mocks.sh
+++ Tests/scripts/upload_mocks.sh
@@ -2,6 +2,8 @@
 set -e
 
 cd content-test-data
+git stash -u
 git checkout mocks-testing
+git stash pop
 git add *
 git commit -m "Updated mock files from content branch '$1' build number - $2" && git push --force || :
