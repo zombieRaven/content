9f57754d5e615f47bb7a323048609fa58f5c10cd
Avidan Hessing <ahessing@paloaltonetworks.com>
2020-01-01 17:16:21 +0200

    switch to branch after cloning mocks

--- Tests/scripts/clone_mocks.sh
+++ Tests/scripts/clone_mocks.sh
@@ -5,4 +5,5 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 if [[ ! -d "content-test-data" ]]; then
     git clone git@github.com:demisto/content-test-data.git
+    git checkout mocks-testing
 fi
--- Tests/scripts/upload_mocks.sh
+++ Tests/scripts/upload_mocks.sh
@@ -3,7 +3,4 @@ set -e
 
 cd content-test-data
 git add *
-git stash
-git checkout mocks-testing
-git stash pop
 git commit -m "Updated mock files from content branch '$1' build number - $2" && git push --force || :
