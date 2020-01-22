6e871d70e1f615b7807f22380403ae1a67e9ce68
syaakovi <syaakovi@paloaltonetworks.com>
2020-01-22 13:54:36 +0200

    added option to run content tests against specific server build number

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -2,29 +2,32 @@ ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$1"
 
-if [ -n "${SERVER_BRANCH_NAME}" ]
+if [ -z "${ARTIFACT_BUILD_NUM}" ]
   then
-    _server_branch_name=${SERVER_BRANCH_NAME}
+    # defaults to latest master build
+    if [ -n "${SERVER_BRANCH_NAME}" ]
+      then
+        _server_branch_name=${SERVER_BRANCH_NAME}
 
-  else
-    _server_branch_name="master"
-
-fi
-
-echo "Getting latest build num"
-TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/${_server_branch_name}?limit=10&filter=successful&$TOKEN_ATTR")
-
-ARTIFACT_BUILD_NUM=
-for i in `seq 0 9`; do
-    if [[ $(echo "$TEMP" | jq ".[$i].build_parameters") == "null" ]]; then
-        ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq ".[$i].build_num")
-        break
+      else
+        _server_branch_name="master"
     fi
-done
 
-if [[ "$ARTIFACT_BUILD_NUM" = "" ]]; then
-    echo "couldn't find successful build"
-    exit 1
+    echo "Getting latest build num"
+    TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/${_server_branch_name}?limit=10&filter=successful&$TOKEN_ATTR")
+
+    ARTIFACT_BUILD_NUM=
+    for i in `seq 0 9`; do
+        if [[ $(echo "$TEMP" | jq ".[$i].build_parameters") == "null" ]]; then
+            ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq ".[$i].build_num")
+            break
+        fi
+    done
+
+    if [[ "$ARTIFACT_BUILD_NUM" = "" ]]; then
+        echo "couldn't find successful build"
+        exit 1
+    fi
 fi
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/ | head -n 1)
